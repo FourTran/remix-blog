364038d7b561ecfc80ee90e2fb696d305c0a65c2
Four <tranvantubk@gmail.com>
2024-07-19 13:24:10 +0700

    init project
    set up remix project

---
+++ app/routes/_index.tsx
@@ -0,0 +1,33 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">Welcome to Remix</h1>
+      <ul className="list-disc mt-4 pl-6 space-y-2">
+        <li>
+          <Link
+            className="text-blue-700 underline visited:text-purple-900"
+            to="/jokes"
+            rel="noreferrer"
+          >
+            Jokes
+          </Link>
+        </li>
+        <li>
+          <Link reloadDocument to="/jokes.rss">
+            RSS
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
