0c8b38cb4034dbebfbcbe82b283584a4471f35b4
Four <tranvantubk@gmail.com>
2024-07-19 14:18:06 +0700

    config layout and add default metadata

--- app/root.tsx
+++ app/root.tsx
@@ -1,26 +1,46 @@
 import {
   Links,
+  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
+import { MetaFunction } from "@remix-run/node";
 import "./tailwind.css";
+import type { PropsWithChildren } from "react";
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+function Document({
+  children,
+  title = "Remix: So great, it's funny!",
+}: PropsWithChildren<{ title?: string }>) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="Remix,jokes" />
+        <meta
+          name="twitter:image"
+          content="https://remix-jokes.lol/social.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@remix_run" />
+        <meta name="twitter:site" content="@remix_run" />
+        <meta name="twitter:title" content="Remix Jokes" />
         <Meta />
+        {title ? <title>{title}</title> : null}
         <Links />
       </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
@@ -28,3 +48,27 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return <Outlet />;
 }
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  const errorMessage = error instanceof Error ? error.message : "Unknown error";
+  return (
+    <Document title="Uh-oh!">
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{errorMessage}</pre>
+      </div>
+    </Document>
+  );
+}
+
+export const meta: MetaFunction = () => {
+  const description = "Learn Remix and laugh at the same time!";
+
+  return [
+    { name: "description", content: description },
+    { name: "twitter:description", content: description },
+    { title: "Remix: So great, it's funny!" },
+  ];
+};
---
+++ app/siteMetadata.tsx
@@ -0,0 +1,18 @@
+export const siteMetadata = {
+  title: "Remix blog",
+  description:
+    "This a Remix template for a blog. Check the README on GitHub to see how to build your own blog using this template.",
+  domain: "#",
+  url: "#",
+  image: "",
+  author: "Four Tran",
+  github: "FourTran",
+  linkedin: "/in/four-tran/",
+  email: "tranvantu4444@gmail.com",
+  professionalTitle: "Senior Software Engineer",
+  aboutMe:
+    "I'm a Senior Software Engineer. More than 9 years of experience developing software. International working experience. Full-Stack knowledge with a focus on web development. Interested in new technologies and cybersecurity.",
+  logo: "https://avatars.githubusercontent.com/u/13285951?s=400&u=d0bfb1e5f5629d6adbef3b54d1b831db1aa155ab&v=4",
+  logo_dark_mode:
+    "https://avatars.githubusercontent.com/u/13285951?s=400&u=d0bfb1e5f5629d6adbef3b54d1b831db1aa155ab&v=4",
+};
