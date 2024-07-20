1602110d8a8362752d869df1f8053a98565fb8e3
Four <tranvantubk@gmail.com>
2024-07-20 10:04:21 +0700

    Update header.tsx
    change blogs header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,53 +6,55 @@ import siteLogo from "~/assets/logo-text.png";
 import { AvatarUser } from "./avatar";
 export const Header = ({ user }: { user: any }) => {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-          to="/"
-        >
-          <img src={siteLogo} className="h-8" alt="Flowbite Logo" />
-        </Link>
-        <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"></div>
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-user"
-        >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                to="/blogs"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                aria-current="page"
-              >
-                Blogs
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/admin"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Admin
-              </Link>
-            </li>
-            <li>
-              {user ? (
-                <div className="user-info">
-                  <form action="/logout" method="post">
-                    <button type="submit" className="button">
-                      Logout
-                    </button>
-                  </form>
-                </div>
-              ) : (
-                <Link to="/login">Login</Link>
-              )}
-            </li>
-          </ul>
+    <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+            to="/"
+          >
+            <img src={siteLogo} className="h-8" alt="Logo" />
+          </Link>
+          <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"></div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-user"
+          >
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link
+                  to="/blogs"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  aria-current="page"
+                >
+                  Blogs
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/admin"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Admin
+                </Link>
+              </li>
+              <li>
+                {user ? (
+                  <div className="user-info">
+                    <form action="/logout" method="post">
+                      <button type="submit" className="button">
+                        Logout
+                      </button>
+                    </form>
+                  </div>
+                ) : (
+                  <Link to="/login">Login</Link>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
